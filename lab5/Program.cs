6b7787716df248d423ab7aba01e0ee0c6e02fb8d
4ERILYA <chernikilya2002@gmail.com>
2023-03-18 13:39:50 +0300

    init

---
+++ lab5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "C:\\Users\\chern\\Desktop\\input.txt"; // путь к файлу
+        int sum = 0; // общая сумма чисел
+        int count = 0; // количество чисел
+
+        // Чтение файла построчно
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                // Удаление пробелов в начале и конце строки
+                line = line.Trim();
+
+                // Попытка преобразования строки в число
+                int number;
+                if (int.TryParse(line, out number))
+                {
+                    sum += number;
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine("Сумма чисел: " + sum);
+        Console.WriteLine("Количество чисел: " + count);
+    }
+}
